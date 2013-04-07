5c1041208a1f70598f59ed5dfdb2b5f6ccec4c06
Zachery Metcalf <zachery.metcalf@gmail.com>
2013-04-07 14:54:13 -0600

    Moved Files, integrating to pulse

---
+++ components/rebuildsearch/src/java/org/torweg/pulse/component/rebuildsearch/RebuildSearch.java
@@ -0,0 +1,48 @@
+package org.torweg.pulse.component.rebuildsearch;
+
+//Code snippets from 6.3.1 of Hibernate Search
+//http://docs.jboss.org/hibernate/search/3.2/reference/en-US/html/manual-index-changes.html
+
+import org.hibernate.CacheMode;
+import org.hibernate.FlushMode;
+import org.hibernate.ScrollableResults;
+import org.hibernate.search.Search;
+import org.hibernate.search.FullTextSession;
+import org.hibernate.ScrollMode;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import org.torweg.pulse.invocation.lifecycle.Lifecycle;
+
+public class RebuildSearch {
+
+    public RebuildSearch() {
+    }
+
+    public void rebuild() {
+        Session session = Lifecycle.getHibernateDataSource().creatNewSession();
+        
+        FullTextSession fullTextSession = Search.getFullTextSession(session);
+        fullTextSession.setFlushMode(FlushMode.MANUAL);
+        fullTextSession.setCacheMode(CacheMode.IGNORE);
+        
+        Transaction transaction = fullTextSession.beginTransaction();
+        //Scrollable results will avoid loading too many objects in memory
+        
+        ScrollableResults results = fullTextSession.createCriteria( Email.class )
+            .setFetchSize(BATCH_SIZE)
+            .scroll( ScrollMode.FORWARD_ONLY );
+            
+        int index = 0;
+        
+        while( results.next() ) {
+            index++;
+            fullTextSession.index( results.get(0) ); //index each element
+            if (index % BATCH_SIZE == 0) {
+                fullTextSession.flushToIndexes(); //apply changes to indexes
+                fullTextSession.clear(); //free memory since the queue is processed
+            }
+        }
+        transaction.commit();
+    }
+}
---
+++ components/rebuildsearch/src/java/org/torweg/pulse/component/rebuildsearch/RebuildSearchController.java
@@ -0,0 +1,28 @@
+package org.torweg.pulse.component.rebuildsearch;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.torweg.pulse.annotations.Action;
+import org.torweg.pulse.annotations.AnyAction;
+import org.torweg.pulse.bundle.Controller;
+
+public class RebuildSearchController extends Controller {
+    
+    private static final Logger Logger = LoggerFactory
+        .getLogger(RebuildSearchController.class);
+        
+    @Action("rebuildSearch")
+    public final void rebuildHibernateSearch() {
+    
+        RebuildSearch rebuilder = new RebuildSearch();
+        rebuilder.rebuild();
+    
+    }
+    @AnyAction
+    public final void alwaysRun() {
+        LOGGER.info("success of rebuild");
+    }
+    
+
+}
